909059154dc8f59a10bca459530731ede929ccd8
CelestineAkpanoko <celestineakpanoko@gmail.com>
2021-06-16 20:15:17 +0100

    setting up sever with express

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const talkToWizbot = require("./wizbot");
+var jsonParser = bodyParser.json();
+var urlEncoded = bodyParser.urlenconded({extended: true});
+
+app.use(cors());
+app.use(morgan("dev"));
+app.post("/wizbot",jsonParser, urlEncoded, function(req,res,next){
+    const message = req.body.message;
+    console.log("message" + message);
+
+    talkToWizbot(message)
+    .then((response) => {
+        res.send({message: response});
+    })
+    .catch((error) => {
+        console.log("Something went wrong: "+error);
+        res.send({
+            error: "Error occured here"
+        });
+        });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>{
+    console.log(`Listening on port: ${port}`);
+});
